bb3b14457bd6ebb834016c78d981954d2c989669
Tyler Whitney <tywhitney@PC17.cis.com>
2016-11-01 11:23:59 -0500

    add Team 8

--- team8.py
+++ team8.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'The Spooks' # Only 10 chars displayed.
+strategy_name = 'Lucky winner'
+strategy_description = 'We are playing the luck game randomly selecting hoping for the best'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -25,8 +25,13 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    choice = ''
+    if len (my_history) == 8:
+        choice = 'b' 
+        print(my_history)
+    return choice
+
+
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
@@ -53,7 +58,7 @@ if __name__ == '__main__':
               my_score=0,
               their_score=0,
               result='b'):
-         print 'Test passed'
+         print ('Test passed')
      # Test 2: Continue betraying if they collude despite being betrayed.
     test_move(my_history='bbb',
               their_history='ccc', 
